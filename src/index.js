1cb3d1830b02131e95df8b8944ba49f0089ec5d6
Your Name <you@example.com>
2020-10-24 14:03:08 -0500

    server initialized

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("ra")
+const express = require('express');
+
+const app = express();
+
+/* Importing routes */
+
+import projectRoutes from './routes/projects';
+import taskRoutes from './routes/projects';
+
+/* Settings */
+
+app.set('port', process.env.PORT || 3000);
+
+/* Middlewares */
+
+app.use(express.json());
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+
+/* Server initialization */
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
